d20e194a81629df617947520a48d37e85701845a
mihee <mihee@MAGAPC>
2023-02-15 21:17:29 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Calc("4+32+6 51");
+        }
+         static void Calc(string str) {
+            string[] data = toData(str);
+            foreach (string s in data)
+            {
+                //Console.WriteLine(s, "\n");
+            }
+        }
+
+        static string[] toData(string str)
+        {
+            string[] data = str.Split("");
+
+          
+            foreach (string el in data)
+            {
+                
+                    Console.WriteLine(el+" el");
+            
+            }
+            //" /[^+\\d] / g"
+            return data;
+
+        }
+
+     
+    }
+}
